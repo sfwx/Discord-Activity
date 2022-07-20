875dd7eb33878a29ab3836586335edbccd3249c3
FlowniX <92480881+FlowniX@users.noreply.github.com>
2022-07-20 13:11:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const discord = require('discord.js-selfbot-v11');
+const client = new discord.Client();
+const config = require('./status.json');
+const fs = require('fs');
+const events = fs.readdirSync('./events/');
+events.forEach(file => {
+  const eventname = file.split('.')[0];
+  const event = require(`./events/${file}`);
+client.on(eventname, event.bind(null, client));
+});
+
+client.login(config.token);
+
+
+
+
