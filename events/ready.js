e01e708b65dff8a13afac6a250e225b8a56ddd63
FlowniX <92480881+FlowniX@users.noreply.github.com>
2022-07-20 13:33:02 -0300

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,35 @@
+const rpc = require("discordrpcgenerator");
+const config = require(".././status.json")
+
+module.exports = async(client) => {
+  const small = await rpc.getRpcImage(config.applicationid, config.assets.smallimage.key);
+  rpc.getRpcImage(config.applicationid, config.assets.largeimage.key)
+  .then(large => {
+    let presence = new rpc.Rpc()
+    .setApplicationId(config.applicationid)
+    .setType(config.type)
+    .setUrl(config.url)
+    .setName(config.name)
+    .setDetails(config.details)
+    .setState(config.state)
+    .setAssetsLargeImage(large.id)
+    .setAssetsLargeText(config.assets.largeimage.text)
+    .setAssetsSmallImage(small.id)
+    .setAssetsSmallText(config.assets.smallimage.text)
+    .setStartTimestamp(config.time || Date.now())
+    client.user.setStatus(config.status);
+    client.user.setPresence(presence.toDiscord()).catch(console.error);
+  });
+  console.log("❯ Seu status de natividade foi iniciado com sucesso!");
+}
+
+
+
+
+
+
+
+
+
+
+
