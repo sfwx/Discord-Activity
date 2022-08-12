7bb4bd457f76153bb1371823cc25915bfdf605b3
FlowniX <92480881+TCDhub@users.noreply.github.com>
2022-08-12 04:26:41 -0300

    Update others.sh

--- coding/others.sh
+++ coding/others.sh
@@ -1,5 +1,5 @@
-mv Discord-Activity/coding/index.js Discord-Activity/coding/events Discord-Activity/coding/status.json Discord-Activity/coding/others.sh .
-rm -r Discord-Activity
+mv Discord-Activity/coding/index.js Discord-Activity/coding/events Discord-Activity/coding/status.json .
 npm init -y
 npm install discord.js discord.js-selfbot-v11 discordrpcgenerator
 clear
+rm -r Discord-Activity
