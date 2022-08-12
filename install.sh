cc3d0b06045563cbafa6c4ba7b8525995c4179d8
FlowniX <92480881+TCDhub@users.noreply.github.com>
2022-08-11 23:42:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
-rm -r status/.git status/README.md
+cd status
+rm -r /.git README.md
 npm init -y
 npm install discord.js discord.js-selfbot-v11 discordrpcgenerator
-rm status/install.sh
+rm install.sh
+cd ..
 node status/index.js
