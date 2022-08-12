519089b869a5a90182da1535dc2296ca0dc320ed
FlowniX <92480881+TCDhub@users.noreply.github.com>
2022-08-12 05:14:46 -0300

    Update install.sh

--- termux/install.sh
+++ termux/install.sh
@@ -1,8 +1,9 @@
 rm -r Discord-Activity
 cd stt
+rm -r README.md
 npm init -y
 npm install discord.js discord.js-selfbot-v11 discordrpcgenerator
-rm install.sh README.md
+rm install.sh
 nano status.json
 clear
 node index.js
