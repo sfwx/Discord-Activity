bd93f121c9ad4d9f3891695154a39de43901e777
FlowniX <92480881+TCDhub@users.noreply.github.com>
2022-08-12 04:22:00 -0300

    Update and rename others.md to others.sh

--- coding/others.sh
+++ coding/others.sh
@@ -1,9 +1,5 @@
 mv Discord-Activity/coding/index.js Discord-Activity/coding/events Discord-Activity/coding/status.json Discord-Activity/coding/others.sh .
 rm -r Discord-Activity
-cd status
-rm -r .git README.md
 npm init -y
 npm install discord.js discord.js-selfbot-v11 discordrpcgenerator
 clear
-node index.js
-cd ..
